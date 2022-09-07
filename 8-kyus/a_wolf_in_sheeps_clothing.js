ff59073fdc427411b2459d1aa8303438f119b5c9
rvitality <vital.renzjohn@gmail.com>
2022-09-08 07:47:54 +0800

    complete kata: A wolf's in sheep's clothing

--- 8-kyus/a_wolf_in_sheeps_clothing.js
+++ 8-kyus/a_wolf_in_sheeps_clothing.js
@@ -1,7 +1,14 @@
 // https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15/train/javascript
 
 // ! 1.  A wolf in sheep's clothing ---------------
-const warnTheSheep = queue => {};
+const warnTheSheep = queue => {
+    const reversedQueue = queue.reverse();
+    const wolfsPosition = reversedQueue.findIndex(animal => animal === "wolf");
+
+    return wolfsPosition === 0
+        ? "Pls go away and stop eating my sheep"
+        : `Oi! Sheep number ${wolfsPosition}! You are about to be eaten by a wolf!`;
+};
 
 // console.log(
 //     warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"])
