60c22340277b31091d91c93c3e5fb6651b4bfbbf
rvitality <vital.renzjohn@gmail.com>
2022-09-08 07:31:52 +0800

    add setup files

---
+++ 8-kyus/a_wolf_in_sheeps_clothing.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15/train/javascript
+
+// ! 1.  A wolf in sheep's clothing ---------------
+const warnTheSheep = queue => {};
+
+// console.log(
+//     warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"])
+// );
+// "Oi! Sheep number 2! You are about to be eaten by a wolf!"
+
+// console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"]));
+// "Oi! Sheep number 5! You are about to be eaten by a wolf!"
+
+// console.log(warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep", "sheep", "sheep"]));
+// "Oi! Sheep number 6! You are about to be eaten by a wolf!"
+
+// ! solutions
+// https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15/solutions/javascript
