62cfa09c4b2c4e7e7ea1e5bb75af21b1bf136a67
rvitality <vital.renzjohn.com>
2022-09-14 22:51:30 +0800

    completed: Square(n) Sum

---
+++ 8-kyus/square_sum.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+
+// ! Square(n) Sum ---------------
+const squareSum = numbers => numbers.reduce((sum, num) => sum + num ** 2, 0);
