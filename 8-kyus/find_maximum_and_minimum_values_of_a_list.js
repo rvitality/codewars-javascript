37bacafc465ce2c61866e848a8deef154f9170b0
rvitality <vital.renzjohn@gmail.com>
2022-09-16 22:38:42 +0800

    completed: Find Maximum and Minimum Values of a List

---
+++ 8-kyus/find_maximum_and_minimum_values_of_a_list.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
+
+// ! Find Maximum and Minimum Values of a List ---------------
+const min = list => Math.min(...list);
+const max = list => Math.max(...list);
