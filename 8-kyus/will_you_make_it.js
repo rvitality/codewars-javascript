5c036f14bdf3efca0688e5ebaa531b9d5260c6c8
rvitality <vital.renzjohn@gmail.com>
2022-09-16 22:33:27 +0800

    completed: Will you make it?

---
+++ 8-kyus/will_you_make_it.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+
+// ! Will you make it? ---------------
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
