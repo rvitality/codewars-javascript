429d245771928c4fb780510430073d959aee5928
rvitality <vital.renzjohn@gmail.com>
2022-09-17 09:03:27 +0800

    completed: Do I get a bonus?

---
+++ 8-kyus/bonus_time.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+// ! Do I get a bonus? ---------------
+const bonusTime = (salary, bonus) => (bonus ? `£${salary * 10}` : `£${salary}`);
