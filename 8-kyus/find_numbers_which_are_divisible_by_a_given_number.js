82f8abe30ffe60cab0417dbfc85919ed28a3338d
rvitality <vital.renzjohn@gmail.com>
2022-09-17 10:14:05 +0800

    completed: Find numbers which are divisible by a given number

---
+++ 8-kyus/find_numbers_which_are_divisible_by_a_given_number.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/55edaba99da3a9c84000003b/train/javascript
+
+// ! Find numbers which are divisible by given number ---------------
+const divisibleBy = (numbers, divisor) => numbers.filter(num => num % divisor === 0);
