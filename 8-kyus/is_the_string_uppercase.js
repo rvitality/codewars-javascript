1344234afbb7c1b1255ec2ada88b4d785e168742
rvitality <vital.renzjohn@gmail.com>
2022-09-18 09:57:53 +0800

    completed: Is the string uppercase?

---
+++ 8-kyus/is_the_string_uppercase.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/javascript
+
+// ! Find the first non-consecutive number ---------------
+
+String.prototype.isUpperCase = function () {
+    const word = `${this}`;
+
+    return word.toUpperCase() === word;
+};
+
+console.log("hello I AM DONALD".isUpperCase());
+console.log("HI".isUpperCase());
