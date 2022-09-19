ed94806a4c3170fedbe247267bee164e144d4762
rvitality <vital.renzjohn@gmail.com>
2022-09-20 05:15:37 +0800

    completed: Simple Multiplication

---
+++ 8-kyus/simple_multiplication.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+
+// ! Simple multiplication ---------------
+const simpleMultiplication = num => (num % 2 === 0 ? num * 8 : num * 9);
