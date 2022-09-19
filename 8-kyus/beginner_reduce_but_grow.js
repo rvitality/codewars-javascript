35557ce2eb740089e34a07341cacb3b51268deb0
rvitality <vital.renzjohn@gmail.com>
2022-09-20 05:14:15 +0800

    completed: Reduce but Grow

---
+++ 8-kyus/beginner_reduce_but_grow.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
+// ! Beginner - Reduce but Grow ---------------
+const grow = arr => arr.reduce((product, num) => product * num, 1);
