4574cbb16ef7489196368f14e5445d0820da8329
rvitality <vital.renzjohn@gmail.com>
2022-09-22 06:55:48 +0800

    completed: List filtering

---
+++ 7-kyus/list_filtering.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
+
+// ! List Filtering ---------------
+
+const filter_list = list => list.filter(item => Number.isInteger(item));
