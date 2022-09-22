72ccd25b9e54522e58cc4deb2605548b307dc471
rvitality <vital.renzjohn@gmail.com>
2022-09-23 07:49:10 +0800

    Completed: Growth of a Population

--- 7-kyus/growth_of_a_population.js
+++ 7-kyus/growth_of_a_population.js
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript
 
-// ! Friend or Foe?  ---------------
+// ! Growth of a Population  ---------------
 
 const nbYear = (initialPopulation, percent, aug, populationTosurpass) => {
     const percentage = percent / 100;
