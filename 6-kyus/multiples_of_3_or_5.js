1a6ded20cdf8190031c920709499d66620081ccc
rvitality <vital.renzjohn@gmail.com>
2022-10-02 09:46:13 +0800

    Completed: Multiples of 3 or 5

---
+++ 6-kyus/multiples_of_3_or_5.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+
+// ! Multiples of 3 or 5 ---------------
+const solution = number => {
+    let sum = 0;
+
+    for (let i = 1; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+};
