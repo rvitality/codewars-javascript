c758e6eb394b82db9e363d887271e19e93e3ec5f
rvitality <vital.renzjohn@gmail.com>
2022-10-06 06:59:25 +0800

    Completed: Round up to the next multiple of 5

---
+++ 7-kyus/round_up_to_the_next_multiple_of_five.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/55d1d6d5955ec6365400006d/train/javascript
+
+// ! Round up to the next multiple of 5 ---------------
+
+const roundToNext5 = num => Math.ceil(num / 5) * 5;
