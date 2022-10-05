2f465d6de46e03676be0707e0b0c5186e875d17a
rvitality <vital.renzjohn@gmail.com>
2022-10-06 07:02:34 +0800

    Completed: Sort array by string length

---
+++ 7-kyus/sort_array_by_string_length.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/javascript
+
+// ! Sort array by string length ---------------
+
+const sortByLength = array => array.sort((a, b) => a.length - b.length);
