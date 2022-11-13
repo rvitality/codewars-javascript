53d46846cc030a82dc7fc97e278d9f2544cfc143
rvitality <vital.renzjohn@gmail.com>
2022-11-13 11:35:07 +0800

    Completed: Twice as old

---
+++ 8-kyus/twice_as_old.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+// ! Twice as old ---------------
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - sonYearsOld * 2);
+}
