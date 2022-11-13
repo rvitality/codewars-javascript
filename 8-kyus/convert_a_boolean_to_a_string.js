f7371772697fee7b2e3425c096df878bc5ae2a29
rvitality <vital.renzjohn@gmail.com>
2022-11-13 11:03:39 +0800

    Completed: Convert a boolean to a string

---
+++ 8-kyus/convert_a_boolean_to_a_string.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+// ! Convert a Boolean to a String ---------------
+
+function booleanToString(b) {
+    return `${b}`;
+}
