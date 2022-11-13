65f074eaac13c881e411adf04ebc63742f68ae62
rvitality <vital.renzjohn@gmail.com>
2022-11-13 11:20:29 +0800

    Completed: Removing elements

---
+++ 8-kyus/removing_elements.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/javascript
+
+// ! Removing Elements ---------------
+
+const removeEveryOther = arr => arr.filter((el, index) => index % 2 === 0);
