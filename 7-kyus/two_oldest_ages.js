4227053a0ebd3ada6165e3037ded03065961d118
rvitality <vital.renzjohn@gmail.com>
2022-12-02 07:08:37 +0800

    Completed: Two oldest ages

---
+++ 7-kyus/two_oldest_ages.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/511f11d355fe575d2c000001/train/javascript
+
+// ! Two Oldest Ages ---------------
+
+const twoOldestAges = ages => ages.sort((a, b) => a - b).slice(-2);
