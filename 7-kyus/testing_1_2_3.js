6fd887eb9d144743d67a3fa3c957091fe230a8bf
rvitality <vital.renzjohn@gmail.com>
2022-12-06 20:23:10 +0800

    Completed: Testing 1, 2, 3

---
+++ 7-kyus/testing_1_2_3.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+
+// ! Testing 1-2-3 ---------------
+const number = arr => arr.map((el, index) => `${index + 1}: ${el}`);
