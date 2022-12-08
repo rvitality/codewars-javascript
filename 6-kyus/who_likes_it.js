60103aa4fad429d548c36fa0d4a783332202c5f5
rvitality <vital.renzjohn@gmail.com>
2022-12-08 21:17:46 +0800

    Completed: Who likes it?

---
+++ 6-kyus/who_likes_it.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+
+// ! Multiples of 3 or 5 ---------------
+const likes = names => {
+    if (names.length === 0) return "no one likes this";
+    if (names.length === 1) return `${names[0]} likes this`;
+    if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+    if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    if (names.length >= 4)
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+};
