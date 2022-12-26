8bbf3f1ae62b069ade75117ed9da399038863014
rvitality <vital.renzjohn@gmail.com>
2022-12-26 23:32:14 +0800

    Completed: Larget 5 digit number in a series

---
+++ 7-kyus/largest_five_digit_number_in_a_series.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/51675d17e0c1bed195000001/train/javascript
+
+// ! Largest 5 digit number in a series ---------------
+
+const solution = digits => {
+    let biggestNum = 0;
+    for (let i = 0; i <= digits.length - 5; i++) {
+        const nextNum = digits.slice(i, i + 5);
+
+        if (+nextNum > biggestNum) {
+            biggestNum = nextNum;
+        }
+    }
+
+    return biggestNum;
+};
