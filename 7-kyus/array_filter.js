f79f36e06f2716ecd0a5483957df1050c45bf165
rvitality <vital.renzjohn@gmail.com>
2023-01-11 22:04:00 +0800

    Completed: Array Filter

---
+++ 7-kyus/array_filter.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/514a6336889283a3d2000001/train/javascript
+
+// ! JavaScript Array Filter ---------------
+
+const getEvenNumbers = arr => arr.filter(num => num % 2 === 0);
