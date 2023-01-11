ef2c1a438f36b9afe58b1826752eb4a8f377aceb
rvitality <vital.renzjohn@gmail.com>
2023-01-11 22:00:01 +0800

    Completed: Small Enough?

---
+++ 7-kyus/small_enough.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57cc981a58da9e302a000214/train/javascript
+
+// ! Small enough? - Beginner ---------------
+
+const smallEnough = (arr, limit) => arr.every(num => num <= limit);
