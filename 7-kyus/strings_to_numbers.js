0e617fb9b4951e95bafe60a53008591b4166df6e
rvitality <vital.renzjohn@gmail.com>
2023-01-13 19:33:23 +0800

    Completed: Convert an array of strings to array of numbers

---
+++ 7-kyus/strings_to_numbers.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/5783d8f3202c0e486c001d23/train/javascript
+
+// ! Convert an array of strings to array of numbers ---------------
+const toNumberArray = arr => arr.map(el => +el);
