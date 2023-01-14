105aa1f535aee0f8d33c6e5db6dfa10454a85c71
rvitality <vital.renzjohn@gmail.com>
2023-01-15 00:16:14 +0800

    Completed: Simple beads count

---
+++ 7-kyus/simple_beads_count.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/58712dfa5c538b6fc7000569/train/javascript
+
+// ! Simple beads count ---------------
+
+const countRedBeads = blueBeads => (blueBeads < 2 ? 0 : (blueBeads - 1) * 2);
