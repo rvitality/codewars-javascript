2e0d962b544b4683f7917738a4a432465e58e864
rvitality <vital.renzjohn@gmail.com>
2023-01-14 23:06:19 +0800

    Completed: No oddities here

---
+++ 7-kyus/no_oddities_here.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
+
+// ! No oddities here ---------------
+
+const noOdds = values => values.filter(value => value % 2 === 0);
