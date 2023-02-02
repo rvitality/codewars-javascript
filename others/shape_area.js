1ec81e1882f8a62e0b82777afc105bec430b8568
rvitality <vital.renzjohn@gmail.com>
2023-02-02 20:35:32 +0800

    Completed: Shape Area

---
+++ others/shape_area.js
@@ -0,0 +1,21 @@
+function solution(n) {
+    const prev = n - 1;
+    const sum = n + prev;
+    return sum * n - prev;
+    // 1 -> 1
+    // 2 -> 5
+    // 3 -> 13
+
+    // 4 -> 25
+
+    // 4 - 1 = 3
+    // 4 + 3 = 7
+    // 4 * 7 = 28
+    // 28 - 3 =
+}
+
+/*
+Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+*/
