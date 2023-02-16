8ff1a3bc75654ed8e40ef213c7709f190672f057
rvitality <vital.renzjohn@gmail.com>
2023-02-16 22:39:38 +0800

    Completed: Keep up the hoop

---
+++ 8-kyus/keep_up_the_hoop.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
+
+// ! Keep up the hoop ---------------
+const hoopCount = n => (n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it");
