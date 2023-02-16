b22410d55bbce0e2a1cbca6ac01b775640c6b917
rvitality <vital.renzjohn@gmail.com>
2023-02-16 22:45:10 +0800

    Completed: Terminal game move function

---
+++ 8-kyus/terminal_game_move.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/javascript
+
+// ! Grasshopper - Terminal game move function ---------------
+const move = (position, roll) => roll * 2 + position;
