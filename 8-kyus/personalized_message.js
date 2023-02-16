8a86d3318bd49bd389554e4cf73297e5c4fa4620
rvitality <vital.renzjohn@gmail.com>
2023-02-16 22:37:51 +0800

    Completed: Personalized Message

---
+++ 8-kyus/personalized_message.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/javascript
+
+// ! Grasshopper - Personalized Message ---------------
+const greet = (name, owner) => (name === owner ? "Hello boss" : "Hello guest");
