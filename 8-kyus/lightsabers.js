e23e13fc608681fac4a6c8b957649279f698ba07
rvitality <vital.renzjohn@gmail.com>
2023-02-17 21:22:37 +0800

    Completed: How many lightsabers do you have?

---
+++ 8-kyus/lightsabers.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/solutions/javascript
+
+// ! How many lightsabers do you own? ---------------
+
+function howManyLightsabersDoYouOwn(name) {
+    return name === "Zach" ? 18 : 0;
+}
