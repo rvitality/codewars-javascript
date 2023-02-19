8e4c441d2651b511b3085e27620f5113b4844598
rvitality <vital.renzjohn@gmail.com>
2023-02-19 20:09:08 +0800

    Completed: What's the real floor?

---
+++ 8-kyus/whats_the_real_floor.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+
+// ! What's the real floor? ---------------
+
+const getRealFloor = n => (n <= 0 ? n : n >= 13 ? n - 2 : n - 1);
