0d6f228621b088ca60bb5fbb8beb86c30036910d
rvitality <vital.renzjohn@gmail.com>
2023-02-20 21:23:51 +0800

    Completed: Do you speak english?

---
+++ 8-kyus/do_you_speak_english.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
+
+// ! Do you speak "English"? ---------------
+
+const spEng = sentence => sentence.toLowerCase().includes("english");
