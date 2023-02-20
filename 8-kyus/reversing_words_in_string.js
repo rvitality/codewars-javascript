4c19d475f10628cb0c4089a3de1cb53e99014a69
rvitality <vital.renzjohn@gmail.com>
2023-02-20 21:03:12 +0800

    Completed: Reversing words in a string

---
+++ 8-kyus/reversing_words_in_string.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57a55c8b72292d057b000594/train/javascript
+
+// ! Reversing Words in a String ---------------
+
+const reverse = str => str.split(" ").reverse().join(" ");
