3510684b361ae384eb3e83f6af6a9f4e55b32c4b
rvitality <vital.renzjohn@gmail.com>
2023-02-20 21:08:13 +0800

    Completed: Find the position!

---
+++ 8-kyus/find_the_position.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5808e2006b65bff35500008f/train/javascript
+
+// ! Find the position! ---------------
+
+const position = letter => `Position of alphabet: ${letter.charCodeAt(0) - 96}`;
