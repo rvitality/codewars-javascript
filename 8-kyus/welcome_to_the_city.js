138bd970e77958aa5c1b747c727039c2bbd15427
rvitality <vital.renzjohn@gmail.com>
2023-02-20 21:14:11 +0800

    Completed: Welcome to the City!

---
+++ 8-kyus/welcome_to_the_city.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
+
+// ! Welcome to the City ---------------
+
+const sayHello = (name, city, state) => `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
