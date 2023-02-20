5de34489571429192657735a0b7203525c146ad4
rvitality <vital.renzjohn@gmail.com>
2023-02-20 21:12:06 +0800

    Completed: Regular Ball Super Ball

---
+++ 8-kyus/regular_ball_super_ball.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/javascript
+
+// ! Regular Ball Super Ball ---------------
+
+const Ball = function (ballType) {
+    this.ballType = ballType || "regular";
+};
