526fb079f900957dcf1659fb6a52a609f41e4861
rvitality <vital.renzjohn@gmail.com>
2023-02-22 21:25:26 +0800

    Completed: Five without numbersclear!

---
+++ 8-kyus/five_without_numbers.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+// ! 5 without numbers !! ---------------
+
+function unusualFive() {
+    return "K".charCodeAt() % "F".charCodeAt();
+}
