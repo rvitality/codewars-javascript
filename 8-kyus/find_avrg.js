db9ed740f16e5a32ecdb2f3a04f941e3c85489e0
rvitality <vital.renzjohn@gmail.com>
2023-03-07 06:53:59 +0800

    Completed: Array Mean

---
+++ 8-kyus/find_avrg.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/55d277882e139d0b6000005d/train/javascript
+
+// ! Grasshopper - Array Mean ---------------
+
+const findAverage = nums => nums.reduce((sum, num) => sum + num, 0) / nums.length;
