d911844bf58ad41710051dbabfb59da11dc1e7b5
rvitality <vital.renzjohn@gmail.com>
2023-03-07 06:52:12 +0800

    Completed: Merge Arrays

---
+++ 8-kyus/merge_arrays.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5899642f6e1b25935d000161/train/javascript
+
+// ! Merge two sorted arrays into one ---------------
+
+function mergeArrays(arr1, arr2) {
+    return [...new Set([...arr1, ...arr2])].sort((a, b) => a - b);
+}
