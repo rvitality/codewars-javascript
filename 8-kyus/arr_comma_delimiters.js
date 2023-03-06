5fffba084333916ad36be374ab1cb42c8d5dd94c
rvitality <vital.renzjohn@gmail.com>
2023-03-07 06:57:29 +0800

    Completed: Array with comma delimiters

---
+++ 8-kyus/arr_comma_delimiters.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+
+// ! Printing Array elements with Comma delimiters ---------------
+const printArray = arr => arr.join(",");
