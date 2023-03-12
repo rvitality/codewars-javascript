392b7bd59600ccaa712333daa2e5717f00c5bde1
rvitality <vital.renzjohn@gmail.com>
2023-03-12 20:37:00 +0800

    Completed: Unexpected Parsing

---
+++ 8-kyus/unexpected_parsing.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/54fdaa4a50f167b5c000005f/train/javascript
+
+// ! Unexpected parsing ---------------
+const getStatus = isBusy => ({ status: isBusy ? "busy" : "available" });
