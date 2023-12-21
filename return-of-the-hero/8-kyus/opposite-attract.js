10d31dd592e52f793fe773e67239554fd4ff610e
rvitality <vital.renzjohn@gmail.com>
2023-12-21 22:55:09 +0800

    ROTH: Opposites Attract

---
+++ return-of-the-hero/8-kyus/opposite-attract.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+// ! Abbreviate a Two Word Name --------------
+
+const lovefunc = (flower1, flower2) => {
+    return flower1 % 2 !== flower2 % 2;
+};
