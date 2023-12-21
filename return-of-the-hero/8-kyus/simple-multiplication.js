ae490fd7aabcfc947baf98f2197ac623575a75bc
rvitality <vital.renzjohn@gmail.com>
2023-12-21 23:06:58 +0800

    ROTH: Simple Multiplication

---
+++ return-of-the-hero/8-kyus/simple-multiplication.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+
+// ! Simple multiplication --------------
+
+const simpleMultiplication = (number) => (number % 2 ? number * 9 : number * 8);
