dde8f59ee67a302e5a971cdc3c7974efa9466332
rvitality <vital.renzjohn@gmail.com>
2023-12-30 19:03:41 +0800

    Completed: Switch it Up

---
+++ return-of-the-hero/8-kyus/switch-it-up.js
@@ -0,0 +1,16 @@
+function switchItUp(number) {
+    const words = [
+        "Zero",
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+    ];
+
+    return words[number];
+}
