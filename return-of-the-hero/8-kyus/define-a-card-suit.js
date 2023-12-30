4142b1b5b7bb2b1e657d98bc4e09a1a949f5edc0
rvitality <vital.renzjohn@gmail.com>
2023-12-30 19:07:38 +0800

    Completed: Define a card suit

---
+++ return-of-the-hero/8-kyus/define-a-card-suit.js
@@ -0,0 +1,14 @@
+function defineSuit(card) {
+    if (card.includes("♣")) {
+        return "clubs";
+    }
+    if (card.includes("♦")) {
+        return "diamonds";
+    }
+    if (card.includes("♥")) {
+        return "hearts";
+    }
+    if (card.includes("♠")) {
+        return "spades";
+    }
+}
