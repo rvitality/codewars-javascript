451691e424b55db833852730d6685771533218c5
rvitality <vital.renzjohn@gmail.com>
2024-01-05 20:10:50 +0800

    ROTH: FizzBuzz

---
+++ return-of-the-hero/7-kyus/fizz-buzz.js
@@ -0,0 +1,17 @@
+// Return an array
+function fizzbuzz(n) {
+    const res = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            res.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            res.push("Fizz");
+        } else if (i % 5 === 0) {
+            res.push("Buzz");
+        } else {
+            res.push(i);
+        }
+    }
+
+    return res;
+}
