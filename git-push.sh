948949a394c762ca9798863a2f80cb48ac1e71ea
rvitality <vital.renzjohn@gmail.com>
2025-05-07 21:49:53 +0800

    Automate commit and push

--- git-push.sh
+++ git-push.sh
@@ -3,7 +3,10 @@
 # Prompt for commit message
 read -p "Enter commit message: " message
 
+# Prefix the message
+full_message="THR: $message"
+
 # Run Git commands
 git add .
-git commit -m "$message"
+git commit -m "$full_message"
 git push origin main
